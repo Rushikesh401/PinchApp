f6b65fafc6f48f0b4a37dcc07a6f279b110b6bf7
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-29 14:15:59 +0530

    initial commit

---
+++ PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Rushikesh Suradkar on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
