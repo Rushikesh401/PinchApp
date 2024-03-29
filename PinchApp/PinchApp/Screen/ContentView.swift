3883f609273fcf5c07088519e11692d9ce2d5629
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-29 15:05:45 +0530

    Added front cover image with animation

--- PinchApp/PinchApp/Screen/ContentView.swift
+++ PinchApp/PinchApp/Screen/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: PROPERTY
+    
+    @State private var isAnimating : Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack {
+                // MARK: Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear( perform: {
+                withAnimation(.linear(duration: 3)) {
+                    isAnimating = true
+                    
+                }
+             }
+            )
         }
-        .padding()
+        .navigationViewStyle(.stack)
     }
 }
 
