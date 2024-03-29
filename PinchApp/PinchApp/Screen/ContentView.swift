d219ba247475cd26f069ea974ca657e94454f8c8
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-29 15:51:37 +0530

    added drag gesture to image

--- PinchApp/PinchApp/Screen/ContentView.swift
+++ PinchApp/PinchApp/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     @State private var isAnimating : Bool = false
     @State private var imageScale : CGFloat = 1
+    @State private var imageOffset : CGSize = .zero
     
     var body: some View {
         NavigationView{
@@ -24,8 +25,9 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale, anchor: .center)
-                    //MARK: Tap Gesture to scale image
+                //MARK: Tap Gesture to scale image
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()){
@@ -33,11 +35,24 @@ struct ContentView: View {
                             }
                         }
                         else {
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                //MARK: Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                                
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                        )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -46,10 +61,18 @@ struct ContentView: View {
                     isAnimating = true
                     
                 }
-             }
+            }
             )
         }
         .navigationViewStyle(.stack)
+    }
+    
+    //MARK: Function
+    func resetImageState() {
+       return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
     }
 }
 
